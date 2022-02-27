98c219e561d6674ba0debd59482a26b1c29a86c2
Kian van der Meer <kiansinan@gmail.com>
2022-02-27 17:11:13 +0100

    basic connection

--- marta.go
+++ marta.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello kolin.")
+
+	port := ":" + "2222"
+
+	l, err := net.Listen("tcp", port)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer l.Close()
+
+	conn, err := l.Accept()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		netData, err := bufio.NewReader(conn).ReadString('\n')
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if strings.TrimSpace(string(netData)) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+			return
+		}
+
+		t := time.Now()
+		myTime := t.Format(time.RFC3339) + "\n"
+
+		fmt.Print("-> ", string(netData))
+		conn.Write([]byte(myTime))
+	}
 }
