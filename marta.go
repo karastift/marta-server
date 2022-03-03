7dfe1ecdf4053720bf79cf91b6d39970652e9daf
Kian van der Meer <kiansinan@gmail.com>
2022-03-03 07:01:52 +0100

    solid prototype

--- marta.go
+++ marta.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/karastift/marta-server/home"
 	"github.com/karastift/marta-server/world"
@@ -15,6 +16,19 @@ var clients = world.NewClients()
 
 func main() {
 
+	// TODO:
+	// status command on server
+	// check out memory modules on ghw
+	// check out ghw in general
+	// get right macaddress
+
+	// use real logging package "log"
+
+	// shell access (could be hard)
+	// file download (could be hard)
+	// client sends info with login command
+	// maybe update command (could be hard)
+
 	pool := home.NewPool(clients, 2222)
 
 	go pool.Start()
@@ -22,14 +36,35 @@ func main() {
 	testResponses()
 }
 
+func info() {
+	clients.RequestInfo()
+
+	for _, client := range clients.ClientsArray {
+		fmt.Println(client.Info.String())
+	}
+}
+
 func testResponses() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
+		fmt.Print("> ")
+
 		in, _ := reader.ReadString('\n')
 
-		res := clients.SendWithRes(in)
+		if in == "!info\n" {
+			info()
+			continue
+		}
 
-		fmt.Println(res)
+		responses := clients.SendWithRes(in)
+
+		for _, res := range responses {
+			fmt.Println("'" + strings.TrimRight(res, "\n") + "'")
+		}
+
+		for _, client := range clients.ClientsArray {
+			fmt.Println(client.Conn.RemoteAddr())
+		}
 	}
 }
--- world/clients.go
+++ world/clients.go
@@ -24,7 +24,6 @@ func (clients *Clients) AddClient(newClient Client) {
 
 // Removes `toRemove` (instance of Client) of `ClientsArray`.
 func (clients *Clients) RemoveClient(toRemove Client) {
-
 	var removeIndex int
 
 	for index, client := range clients.ClientsArray {
@@ -39,75 +38,86 @@ func (clients *Clients) RemoveClient(toRemove Client) {
 
 // Send data to all clients without waiting for any response.
 func (clients *Clients) Send(data string) {
+	mapWithWaitGroup(&clients.ClientsArray, func(_ int, curr *Client, wg *sync.WaitGroup) {
+		// decrement waitgroup counter
+		defer wg.Done()
 
-	// waitgroup for waiting until data is sent to all clients
-	var wg sync.WaitGroup
+		// send without waiting for response
+		err := curr.Send(data)
 
-	for _, client := range clients.ClientsArray {
-
-		// increment waitgroup counter
-		wg.Add(1)
-
-		// start goroutine to send to clients concurrently
-		go func(curr Client) {
-			// decrement waitgroup counter
-			defer wg.Done()
-
-			// send without waiting for response
-			err := curr.Send(data)
-
-			if err != nil {
-				panic(err)
-			}
-
-		}(client)
-
-		// wait until counter is zero
-		wg.Wait()
-	}
+		if err != nil {
+			panic(err)
+		}
+	})
 }
 
 // Send data to all clients and wait for response.
 //
 // Uses the `Send(string)` method of `Client`. This results in increasing the `TimeoutCount` of a client if it doesnt repond.
 //
-// After the data got sent to a client the method checks if `TimeoutCount` is bigger than 4 and if it is, the client gets removed from `ClientsArray`.
+// After the data got sent to a client the method checks if `TimeoutCount` is bigger than 2 and if it is, the client gets removed from `ClientsArray`.
 func (clients *Clients) SendWithRes(data string) []string {
 
 	// array that gatheres all responses
 	responses := make([]string, len(clients.ClientsArray))
+	timedOut := make([]Client, 0)
 
-	// waitgroup for waiting until the last client reponded or timed out
+	mapWithWaitGroup(&clients.ClientsArray, func(i int, curr *Client, wg *sync.WaitGroup) {
+
+		// decrement waitgroup counter
+		defer wg.Done()
+
+		// send and get response
+		res, err := curr.SendWithRes(data)
+
+		if err != nil {
+			// if count is to big append to timedOut slice
+			if curr.TimeoutCount > 2 {
+				timedOut = append(timedOut, *curr)
+			}
+		} else {
+			responses = append(responses, string(res))
+		}
+
+		clients.ClientsArray[i] = *curr
+	})
+
+	// remove timed out clients from clientarray
+	for _, client := range timedOut {
+		clients.RemoveClient(client)
+	}
+
+	return responses
+}
+
+// Requests info from client. Updates `Info` and returns it.
+func (clients *Clients) RequestInfo() {
+
+	mapWithWaitGroup(&clients.ClientsArray, func(index int, curr *Client, wg *sync.WaitGroup) {
+		defer wg.Done()
+		curr.RequestInfo()
+
+		// client changed, so change it in array too
+		clients.ClientsArray[index] = *curr
+	})
+}
+
+// Starts the function as a goroutine for every client in the array.
+// The function has to defer call `wg.Done()` when its finished!
+func mapWithWaitGroup(arr *[]Client, fn func(int, *Client, *sync.WaitGroup)) {
+
+	// create waitgroup
 	var wg sync.WaitGroup
 
-	for _, client := range clients.ClientsArray {
+	for i, client := range *arr {
 
 		// increment waitgroup counter
 		wg.Add(1)
 
-		// start goroutine to send to clients concurrently
-		go func(curr Client) {
-
-			// decrement waitgroup counter
-			defer wg.Done()
-
-			// send and get response
-			res, err := curr.SendWithRes(data)
-
-			if err != nil {
-				// if client has timeouted 4 times now, remove it from connected clients
-				if curr.TimeoutCount > 3 {
-					// idk if the defer statement is necessary
-					defer clients.RemoveClient(curr)
-				}
-			} else {
-				responses = append(responses, string(res))
-			}
-		}(client)
+		// call passed function as goroutine
+		go fn(i, &client, &wg)
 
 		// wait until counter is zero
 		wg.Wait()
 	}
-
-	return responses
 }
