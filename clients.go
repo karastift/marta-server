dc544c342cd7ac5bc83520fcc10a5fb7449d6f3d
karastift <69339287+karastift@users.noreply.github.com>
2022-03-04 15:37:00 +0100

    now using map

--- clients.go
+++ clients.go
@@ -5,14 +5,16 @@ import (
 	"sync"
 )
 
+const maxTimeouts int = 2
+
 type Clients struct {
-	ClientsArray []Client
+	clientsMap map[string]Client
 }
 
 // Returns pointer to an instance of Clients.
 func NewClients() *Clients {
 	clients := Clients{
-		ClientsArray: make([]Client, 0),
+		clientsMap: make(map[string]Client),
 	}
 
 	return &clients
@@ -20,37 +22,31 @@ func NewClients() *Clients {
 
 // Returns client with curresponding id.
 func (clients *Clients) GetClientById(id string) (*Client, error) {
-	for _, client := range clients.ClientsArray {
-		if client.Id == id {
-			return &client, nil
-		}
-	}
 
-	return nil, errors.New("could not find client: no id matches the given one")
+	client, ok := clients.clientsMap[id]
+
+	if !ok {
+		return nil, errors.New("could not find client: no id matches the given one")
+
+	} else {
+		return &client, nil
+	}
 }
 
 // Adds `newClient` to the `ClientsArray`.
 func (clients *Clients) AddClient(newClient Client) {
-	clients.ClientsArray = append(clients.ClientsArray, newClient)
+	clients.clientsMap[newClient.Id] = newClient
 }
 
 // Removes `toRemove` (instance of Client) of `ClientsArray`.
 func (clients *Clients) RemoveClient(toRemove Client) {
-	var removeIndex int
-
-	for index, client := range clients.ClientsArray {
-		if client.Equals(toRemove) {
-			removeIndex = index
-			break
-		}
-	}
-
-	clients.ClientsArray = append(clients.ClientsArray[:removeIndex], clients.ClientsArray[removeIndex+1:]...)
+	delete(clients.clientsMap, toRemove.Id)
 }
 
 // Send data to all clients without waiting for any response.
 func (clients *Clients) Send(data string) {
-	mapWithWaitGroup(&clients.ClientsArray, func(_ int, curr *Client, wg *sync.WaitGroup) {
+
+	mapWithWaitGroup(&clients.clientsMap, func(curr *Client, wg *sync.WaitGroup) {
 		// decrement waitgroup counter
 		defer wg.Done()
 
@@ -67,14 +63,14 @@ func (clients *Clients) Send(data string) {
 //
 // Uses the `Send(string)` method of `Client`. This results in increasing the `TimeoutCount` of a client if it doesnt repond.
 //
-// After the data got sent to a client the method checks if `TimeoutCount` is bigger than 2 and if it is, the client gets removed from `ClientsArray`.
+// After the data got sent to a client the method checks if `TimeoutCount` is bigger than `maxTimeouts` and if it is, the client gets removed from `ClientsArray`.
 func (clients *Clients) SendWithRes(data string) []string {
 
 	// array that gatheres all responses
-	responses := make([]string, len(clients.ClientsArray))
+	responses := make([]string, len(clients.clientsMap))
 	timedOut := make([]Client, 0)
 
-	mapWithWaitGroup(&clients.ClientsArray, func(i int, curr *Client, wg *sync.WaitGroup) {
+	mapWithWaitGroup(&clients.clientsMap, func(curr *Client, wg *sync.WaitGroup) {
 
 		// decrement waitgroup counter
 		defer wg.Done()
@@ -84,14 +80,14 @@ func (clients *Clients) SendWithRes(data string) []string {
 
 		if err != nil {
 			// if count is to big append to timedOut slice
-			if curr.TimeoutCount > 2 {
+			if curr.TimeoutCount > maxTimeouts {
 				timedOut = append(timedOut, *curr)
 			}
 		} else {
 			responses = append(responses, string(res))
 		}
 
-		clients.ClientsArray[i] = *curr
+		clients.clientsMap[curr.Id] = *curr
 	})
 
 	// remove timed out clients from clientarray
@@ -105,29 +101,29 @@ func (clients *Clients) SendWithRes(data string) []string {
 // Requests info from client. Updates `Info` and returns it.
 func (clients *Clients) RequestInfo() {
 
-	mapWithWaitGroup(&clients.ClientsArray, func(index int, curr *Client, wg *sync.WaitGroup) {
+	mapWithWaitGroup(&clients.clientsMap, func(curr *Client, wg *sync.WaitGroup) {
 		defer wg.Done()
 		curr.RequestInfo()
 
 		// client changed, so change it in array too
-		clients.ClientsArray[index] = *curr
+		clients.clientsMap[curr.Id] = *curr
 	})
 }
 
 // Starts the function as a goroutine for every client in the array.
 // The function has to defer call `wg.Done()` when its finished!
-func mapWithWaitGroup(arr *[]Client, fn func(int, *Client, *sync.WaitGroup)) {
+func mapWithWaitGroup(arr *map[string]Client, fn func(*Client, *sync.WaitGroup)) {
 
 	// create waitgroup
 	var wg sync.WaitGroup
 
-	for i, client := range *arr {
+	for _, client := range *arr {
 
 		// increment waitgroup counter
 		wg.Add(1)
 
 		// call passed function as goroutine
-		go fn(i, &client, &wg)
+		go fn(&client, &wg)
 
 		// wait until counter is zero
 		wg.Wait()
